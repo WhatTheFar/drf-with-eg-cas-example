545bd5f86b151bd33cc777c4ae24369f474ce6f7
BB-8 <ichannel.jakpat@gmail.com>
2018-07-23 11:17:47 +0700

    Change Cat’s project ALLOW_HOSTS setting

--- cat/cat/settings.py
+++ cat/cat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'stjj3b8i^ar=1s3=piljgd1pe=^$7p5tpw#aseko@n%*m$%hev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'cat']
 
 
 # Application definition
